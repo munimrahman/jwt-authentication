91a163acaf0ada108bf0834a055329fa9f450bf8
munimrahman <munimrh@gmail.com>
2023-05-14 19:39:51 +0600

    redux setup

---
+++ client/src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import apiSlice from "../features/api/apiSlice";
+import authSlice from "../features/auth/authSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    authReducer: authSlice,
+  },
+  middleware: (getDefaultMiddlewares) =>
+    getDefaultMiddlewares().concat(apiSlice.middleware),
+});
+
+export default store;
